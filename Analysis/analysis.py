33f7c659d9e403ce96929599d1a4221b5c486379
bella meng <dancingm1998@gmail.com>
2021-02-16 17:29:28 +0800

    analysis

--- Analysis/analysis.py
+++ Analysis/analysis.py
@@ -2,11 +2,12 @@ import blocksci
 
 
 def main():
-    blocksci_core = input("Get the path of the core data files")
+    blocksci_core = input("Get the path of the core data files: ")
     chain = blocksci.Blockchain(blocksci_core)
-    chain = chain[500000]
+    chain = chain[508241]
     
-    
+    print("Number of addresses: ", chain.address_count(blocksci.address_type.pubkey))
+    print("Number of addresses: ", chain.address_count())
 
 if __name__ == "__main__":
     main()
