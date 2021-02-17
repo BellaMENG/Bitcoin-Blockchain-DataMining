690041b9544b14246dcc7301c9a425e92a304ab2
bella meng <dancingm1998@gmail.com>
2021-02-17 11:34:23 +0800

    analysis

--- Analysis/analysis.py
+++ Analysis/analysis.py
@@ -1,6 +1,17 @@
 import blocksci
 
 
+def get_balances(chain, addresses, balance_file):
+    balances = []
+    for address in addresses:
+        addr = chain.address_from_string(address)
+        balances.append(addr.balance())
+    
+    with open(balance_file, 'w') as f:
+        for balance in balances:
+            f.write("%s\n" % balance)
+
+
 def main():
 #    blocksci_core = input("Get the path of the core data files: ")
     chain = blocksci.Blockchain("/home/zmengaa/blocksci.config")
@@ -13,6 +24,9 @@ def main():
     with open(filename) as f:
         addresses = [line.rstrip() for line in f]
     print("number of addresses:", len(addresses))
+    
+    balance_file = input("file path of balance file: ")
+    get_balances(chain, addresses, balance_file)
     
     
 if __name__ == "__main__":
