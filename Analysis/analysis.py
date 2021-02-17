0e359e5867ac06e9d642b036da71a6b06d950257
bella meng <dancingm1998@gmail.com>
2021-02-17 14:29:30 +0800

    analysis

--- Analysis/analysis.py
+++ Analysis/analysis.py
@@ -12,6 +12,34 @@ def get_balances(chain, addresses, balance_file):
             f.write("%s\n" % balance)
 
 
+def get_txes(chain, addresses, tx_file):
+    txes = []
+    for address in addresses:
+        addr = chain.address_from_string(address)
+        txes.append(len(addr.txes.to_list()))
+        
+    with open(tx_file, 'w') as f:
+        for tx in txes:
+            f.write("%s\n" % tx)
+            
+
+def get_balances_n_txes(chain, addresses, balance_file, tx_file):
+    balances = []
+    txes = []
+    for address in addresses:
+        addr = chain.address_from_string(address)
+        balances.append(addr.balance())
+        txes.append(len(addr.txes.to_list()))
+        
+    with open(balance_file, 'w') as f:
+        for balance in balances:
+            f.write("%s\n" % balance)
+
+    with open(tx_file, 'w') as f:
+        for tx in txes:
+            f.write("%s\n" % tx)
+
+
 def main():
 #    blocksci_core = input("Get the path of the core data files: ")
     chain = blocksci.Blockchain("/home/zmengaa/blocksci.config")
@@ -26,7 +54,8 @@ def main():
     print("number of addresses:", len(addresses))
     
     balance_file = input("file path of balance file: ")
-    get_balances(chain, addresses, balance_file)
+    tx_file = input("file path of tx number file: ")
+    get_balances_n_txes(chain, addresses, balance_file, tx_file)
     
     
 if __name__ == "__main__":
