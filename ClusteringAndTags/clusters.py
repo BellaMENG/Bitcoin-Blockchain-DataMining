35de948e1365467034dfd12cbb8a0db52305484f
bella meng <dancingm1998@gmail.com>
2021-02-19 16:28:11 +0800

    create clusters

--- ClusteringAndTags/clusters.py
+++ ClusteringAndTags/clusters.py
@@ -4,7 +4,9 @@ chain = blocksci.Blockchain("/home/zmengaa/blocksci508241.config")
 dataset_fp = "/home/zmengaa/data500k/txedges/datasets/"
 
 def create_clusters():
+    print("start to create clusters:")
     cm = blocksci.cluster.ClusterManager.create_clustering("blocksci_clusters", chain)
+    print("finish creating clusters!")
     return cm
 
 
