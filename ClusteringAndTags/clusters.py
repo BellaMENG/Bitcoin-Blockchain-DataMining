fb764c074667812610dadc23d548f07e2f284f23
bella meng <dancingm1998@gmail.com>
2021-02-19 16:23:59 +0800

    create clusters

---
+++ ClusteringAndTags/clusters.py
@@ -0,0 +1,21 @@
+import blocksci
+
+chain = blocksci.Blockchain("/home/zmengaa/blocksci508241.config")
+dataset_fp = "/home/zmengaa/data500k/txedges/datasets/"
+
+def create_clusters():
+    cm = blocksci.cluster.ClusterManager.create_clustering("blocksci_clusters", chain)
+    return cm
+
+
+def load_clusters():
+    cm = blocksci.cluster.ClusterManager(dataset_fp + dataset_fp + "blocksci_clusters", chain)
+    return cm
+
+
+def main():
+    create_clusters()
+
+
+if __name__ == "__main__":
+    main()
