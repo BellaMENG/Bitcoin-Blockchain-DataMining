4a04e01a2b2ed1b927c8b3584c82316ddc6f1752
bella meng <dancingm1998@gmail.com>
2021-02-22 10:51:27 +0800

    get number of clusters

--- ClusteringAndTags/clusters.py
+++ ClusteringAndTags/clusters.py
@@ -20,6 +20,7 @@ def load_clusters():
 
 def getClusterIds(output_fn):
     cm = load_clusters()
+    print("Number of clusters:", len(cm.clusters()))
     addr_fn = input("load the addresses, filename: ")
     addr_fn = dataset_fp + addr_fn
     addresses = get_addrs(addr_fn)
